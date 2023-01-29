561bd67d7c74f046ac5ee569a388855d3fbe802c
zenikibeniki <zenikibeniki@gmail.com>
2023-01-29 17:10:11 +0000

    main.js comments

--- main.js
+++ main.js
@@ -1,30 +1,59 @@
-const chargingIcon = document.querySelector("charging-icon");
-const batteryLevel = document.querySelector("battery-level");
-const chargingBar = document.querySelector("charging-bar");
-const dischargingTime = document.querySelector("discharging-time");
-const otherInfo = document.querySelector("other-info");
+// all the containers we need to update the battery information
+
+const chargingIcon = document.querySelector(".charging_icon");
+
+const batteryLevel = document.querySelector(".battery_level");
+
+const chargingBar = document.querySelector(".charging_bar");
+
+const dischargingTime = document.querySelector(".discharging_time");
+
+const otherInfo = document.querySelector(".other_info");
+
+// Getting battery it returns a propmise
 
 navigator.getBattery().then((battery) => {
+  /* Update all the battery information which is a combination of multiple functions */
+
   function updateAllBatteryInfo() {
     updateChargeInfo();
+
     updateLevelInfo();
+
     updateDischargingInfo();
   }
 
+  //   Running as the promise returns battery
+
   updateAllBatteryInfo();
+
+  //  Event Listener, when the charging status changes
+
+  //  it checks that does your device is plugged in or not
 
   battery.addEventListener("chargingchange", function () {
     updateAllBatteryInfo();
   });
+
+  //  Event Listener, when the Battery Level Changes
+
   battery.addEventListener("levelchange", function () {
     updateAllBatteryInfo();
   });
+
+  //  Event Listener, when the discharging Time Change
+
+  //  it checks that does your device is plugged in or not
+
   battery.addEventListener("dischargingtimechange", function () {
     updateAllBatteryInfo();
   });
 
+  //   Updating the battery Level container and the charging bar width
+
   function updateLevelInfo() {
     batteryLevel.textContent = `${parseInt(battery.level * 100)}%`;
+
     chargingBar.style.width = `${parseInt(battery.level * 100)}%`;
   }
 
@@ -38,8 +67,11 @@ navigator.getBattery().then((battery) => {
         (chargingBar.style.animationIterationCount = "initial"));
   }
 
+  //   updating the Discharging Information
+
   function updateDischargingInfo() {
     const dischargeTime = parseInt(battery.dischargingTime / 60) ? true : false;
+
     dischargeTime
       ? ((dischargingTime.textContent = `${parseInt(
           battery.dischargingTime / 60
