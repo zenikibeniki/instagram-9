191ba19b49382192a3499b7881d8474529e26fe4
zenikibeniki <zenikibeniki@gmail.com>
2023-01-29 17:05:09 +0000

    main.js

---
+++ main.js
@@ -0,0 +1,50 @@
+const chargingIcon = document.querySelector("charging-icon");
+const batteryLevel = document.querySelector("battery-level");
+const chargingBar = document.querySelector("charging-bar");
+const dischargingTime = document.querySelector("discharging-time");
+const otherInfo = document.querySelector("other-info");
+
+navigator.getBattery().then((battery) => {
+  function updateAllBatteryInfo() {
+    updateChargeInfo();
+    updateLevelInfo();
+    updateDischargingInfo();
+  }
+
+  updateAllBatteryInfo();
+
+  battery.addEventListener("chargingchange", function () {
+    updateAllBatteryInfo();
+  });
+  battery.addEventListener("levelchange", function () {
+    updateAllBatteryInfo();
+  });
+  battery.addEventListener("dischargingtimechange", function () {
+    updateAllBatteryInfo();
+  });
+
+  function updateLevelInfo() {
+    batteryLevel.textContent = `${parseInt(battery.level * 100)}%`;
+    chargingBar.style.width = `${parseInt(battery.level * 100)}%`;
+  }
+
+  function updateChargeInfo() {
+    battery.charging
+      ? ((chargingBar.style.animationIterationCount = "infinite"),
+        (chargingIcon.style.display = "inline-flex"),
+        (otherInfo.style.display = "none"))
+      : ((chargingIcon.style.display = "none"),
+        (otherInfo.style.display = "inline-flex"),
+        (chargingBar.style.animationIterationCount = "initial"));
+  }
+
+  function updateDischargingInfo() {
+    const dischargeTime = parseInt(battery.dischargingTime / 60) ? true : false;
+    dischargeTime
+      ? ((dischargingTime.textContent = `${parseInt(
+          battery.dischargingTime / 60
+        )} minutes`),
+        (otherInfo.style.display = "flex"))
+      : (otherInfo.style.display = "none");
+  }
+});
